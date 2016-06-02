0629695371364b276ee5b14ea1b40d85c81bfc0b
Dongwoo Kim <idforcoding@gmail.com>
2016-06-02 17:08:16 +0900

    (refactor) Reduce admin pages to single page
    resolve #23

--- server/admin/dbmanage.js
+++ server/admin/dbmanage.js
@@ -1,38 +1,91 @@
-var app = angular.module("dbmanageApp", []);
+angular.module("dbmanageApp", [])
+  .controller("DbmanageController", ["$http", "$window", dbManagerController]);
 
-app.controller("DbmanageController", function($scope, $http) {
-  $scope.mode;
+function dbManagerController($http, $window) {
+  var _this = this;
 
-  $scope.allergic = [['true', '많음'], ['false', '적음']];
-  $scope.absent = [['true', '가능'], ['false', '불가능']];
-  $scope.active = [['true', '활동적'], ['false', '보통']];
-  $scope.single = [['true', '혼자 잘 지냄'], ['false', '외로움을 잘탐']];
-  $scope.friendly = [['true', '강함'], ['false', '약함'], ['default', '보통']];
-  $scope.inside = [['true', '실내'], ['false', '실외'], ['default', '상관없음']];
+  // 사용자의 선택에 따라 편집/추가 mode 설정후, 그에 따른 버튼 기능(submit, goback) 변경 위한 초기 설정.
+  _this.mode = "";
+  _this.submit = undefined;
+  _this.goback = gobackToAdmin;
 
-  $http.get("/puppies")
-    .success(function(response) {
-      $scope.puppies = response;
-    });
+  _this.allergic = [['true', '많음'], ['false', '적음']];
+  _this.absent = [['true', '가능'], ['false', '불가능']];
+  _this.active = [['true', '활동적'], ['false', '보통']];
+  _this.single = [['true', '혼자 잘 지냄'], ['false', '외로움을 잘탐']];
+  _this.friendly = [['true', '강함'], ['false', '약함'], ['default', '보통']];
+  _this.inside = [['true', '실내'], ['false', '실외'], ['default', '상관없음']];
+
+  // server에 db에 저장된 견종 목록을 요청
+  reloadPage();
 
 
-  $scope.new = function() {
+  _this.new = function() {
+    _this.mode = "(추가)";
+    _this.submit = add;
+    _this.goback = gobackToList;
+    _this.puppy = undefined;
+  };
 
-  }
-  $scope.select = function(breed) {
+  _this.select = function(breed) {
+    _this.mode = "(편집)";
+    _this.submit = update;
+    _this.goback = gobackToList;
+    console.log(_this.mode);
     $http.get("/puppies/" + breed)
-      .success(function(response) {
+      .success((response) => {
         console.log('Selected puppy: ', response);
-        $scope.puppy = response;
+        _this.puppy = response;
       });
   };
 
-  // TODO puppy input 유효한지 체크하기(빈칸 등)
-  $scope.update = function(puppy) {
-    $http.put("/puppies/" + puppy._id, puppy)
-      .success(function(response) {
-        console.log("Updated puppy: ", response);
-        $scope.puppies = response;
-      });
+  _this.remove = function(breed) {
+    del(breed);
   };
-});
+
+
+  function update(puppy) {
+    $http.put("/puppies/" + _this.puppy._id, puppy)
+      .success((response) => {
+        console.log("Updated puppy: ", response);
+        _this.puppies = response;
+        _this.mode = "";
+        _this.goback = gobackToAdmin;
+      });
+  }
+
+  function add(puppy) {
+    $http.post("/result", puppy)
+      .success((response) => {
+        console.log("Just added: ", response);
+        _this.puppy = undefined;
+        _this.mode = "";
+        _this.goback = gobackToAdmin;
+        reloadPage();
+      });
+  }
+
+  function del(breed) {
+    $http.delete("/puppies/" + breed)
+      .success((response) => {
+        console.log("Delete succeded. ");
+        _this.puppies = response;
+      });
+  }
+
+  function gobackToList() {
+    _this.mode = "";
+  }
+
+  function gobackToAdmin() {
+    console.log("in goback function");
+    $window.location.href = '/admin';
+  }
+
+  function reloadPage() {
+    $http.get("/puppies")
+      .success((response) => {
+        _this.puppies = response;
+      });
+  }
+}
---
+++ server/admin/deprecated/update.js
@@ -0,0 +1,34 @@
+var app = angular.module("UpdateApp", []);
+
+app.controller("UpdateController", function($scope, $http) {
+
+  $scope.allergic = [['true', '많음'], ['false', '적음']];
+  $scope.absent = [['true', '가능'], ['false', '불가능']];
+  $scope.active = [['true', '활동적'], ['false', '보통']];
+  $scope.single = [['true', '혼자 잘 지냄'], ['false', '외로움을 잘탐']];
+  $scope.friendly = [['true', '강함'], ['false', '약함'], ['default', '보통']];
+  $scope.inside = [['true', '실내'], ['false', '실외'], ['default', '상관없음']];
+
+  $http.get("/puppies")
+    .success(function(response) {
+      $scope.puppies = response;
+    });
+
+
+  $scope.select = function(breed) {
+    $http.get("/puppies/" + breed)
+      .success(function(response) {
+        console.log('Selected puppy: ', response);
+        $scope.puppy = response;
+      });
+  };
+
+  // TODO puppy input 유효한지 체크하기(빈칸 등)
+  $scope.update = function(puppy) {
+    $http.put("/puppies/" + puppy._id, puppy)
+      .success(function(response) {
+        console.log("Updated puppy: ", response);
+        $scope.puppies = response;
+      });
+  };
+});
