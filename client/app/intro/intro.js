44a522bb8f850212ea2daa30dc76e34ab1e7d08b
Minjeong Kim <min7859@gmail.com>
2016-06-04 04:02:59 +0900

    (feat) Replaced to pokemon intro

--- client/app/intro/intro.js
+++ client/app/intro/intro.js
@@ -4,11 +4,24 @@ import 'angular';
 angular
   .module('puppyfinder.intro', []);
 
-// intro controller
-// TODO $scope -> this
 angular
   .module('puppyfinder.intro')
-  .controller('IntroController', [function() {
+  .controller('IntroController', ['$timeout', function($timeout) {
     this.width = window.innerWidth;
     this.height = window.innerHeight;
-  }]);
+
+    this.showMsg = false;
+
+    let bgm = new Audio('../../assets/intro-music.mp3');
+    bgm.addEventListener('ended', () => {
+      bgm.currentTime = 0;
+      bgm.play();
+    });
+
+    angular.element(document.querySelector('video'))[0].onended = () => {
+      bgm.play();
+      $timeout(() => {
+        this.showMsg = true;
+      }, 0);
+    };
+}]);
