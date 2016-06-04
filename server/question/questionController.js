bd972df850c177fb4559d66fd040c959a28dfc94
Dongwoo Kim <idforcoding@gmail.com>
2016-06-04 12:17:38 +0900

    (fix) question order
    fix question order in survey.html
    by adding 'sort' after 'find'

--- server/question/questionController.js
+++ server/question/questionController.js
@@ -2,13 +2,15 @@ import Question from '../question/questionModel';
 
 let api = {
   getAllQuestions: function(req, res) {
-    Question.find({}, function(err, data) {
-      if(err) throw err;
-      else {
-        res.status(200).send(data);
+    Question.find({}).sort({index: 1}).exec(
+      function(err, data) {
+        if(err) throw err;
+        else {
+          res.status(200).send(data);
+        }
       }
-    })
+    );
   }
-}
+};
 
 export default api;
